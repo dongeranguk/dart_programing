e085dc8b382e53df3bf9e1c9749bc3dee587600f
dongeranguk <rooetym@naver.com>
2023-11-28 23:59:48 +0900

    #2 - 비동기 프로그래밍 맛보기
    다트에서 비동기 프로그래밍을 구현하기 위해 코드를 작성 해보았다.
    
    `createUserOrder` 함수는 `fetchUserOrder` 함수가 일정 시간이 지난 뒤 문자열 `Large Latte` 를 리턴 하기 때문에 비동기 함수임을 나타내는 `async` 키워드와 비동기 함수의 작업이 끝날 때까지 기다리도록 `await` 키워드를 사용해야만 비로소 기대한 값이 나타난다.

--- AsyncPrograming.dart
+++ AsyncPrograming.dart
@@ -1,38 +1,28 @@
 void main() {
+  print(createUserOrder());
 
-  // 동기 / 비동기
-  // 동기 : 코드가 순차적으로 실행되는 형태
-  // 비동기 : 모든 코드가 동시 다발적으로 실행되어 순차적인 결과를 보장할 수 없는 형태
+  fetchUserOrder2();
+  print('Fetching user order...');
 
-  // async / await / Future
+  //fetchUserOrder3();
 
-  Future<void> todo(int second) async {
-    await Future.delayed(Duration(seconds: second));
-
-    print('Todo Done in $second seconds');
-  }
-
-  todo(3);
-  todo(1);
-  todo(5);
-
-
-  // async / yield / Stream
-
-  Stream<int> todoStream() async* {
-    int count = 0;
-
-    while(count < 10) {
-      await Future.delayed(Duration(seconds: 1));
-
-      count++;
-      print('TodoStream is Running $count');
-      yield count;
-    }
-
-    print('TodoStream is Done');
-
-  }
-
-  todoStream().listen((event) { });
+  createUserOrder2();
 }
+
+String createUserOrder() {
+  var order = fetchUserOrder();
+  return 'Your order is $order';
+}
+
+Future<void> createUserOrder2() async {
+  var order = await fetchUserOrder4();
+  print('Your order is $order');
+}
+
+Future<String> fetchUserOrder() => Future.delayed(Duration(seconds: 2), () => 'Large Latte');
+
+Future<void> fetchUserOrder2() { return Future.delayed(Duration(seconds: 2), () => print('Large Latte')); }
+
+Future<void> fetchUserOrder3() { return Future.delayed(Duration(seconds: 2), () => throw Exception('Logout failed: user ID is invaild')); }
+
+Future<String> fetchUserOrder4() { return Future.delayed(Duration(seconds: 4), () => 'Large Latte'); }
